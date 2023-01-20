83751fb43c49bfdb8491c0f12bda2dbc3426967d
Андрей Катышев <Andrey-Katyshev@mail.ru>
2023-01-20 08:51:35 +0300

    small edits

--- map_nn/city/views.py
+++ map_nn/city/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 import folium
 
+def style_geo_json(feature):
+    return {
+        'fillColor': 'green',
+        'color': 'green',
+    }
+
 def nn(request):
-    nn = folium.Map(location=[56.2540, 43.9378], zoom_start=11)
+    nn = folium.Map(
+        # width=1800,
+        # height=800,
+        location=[56.2540, 43.9378], 
+        zoom_start=11,
+    )
 
     nn.add_child(folium.LatLngPopup())
 
@@ -37,17 +48,36 @@ def nn(request):
     hutor = folium.Circle(
         radius=800,
         location=[56.2826, 44.0162],
-        popup='Щёлоковский Хутор',
         color='green',
         fill=True,
+        tooltip='Щёлоковский Хутор',
+    ).add_to(parks_and_recreation)
+    folium.PolyLine(
+        locations=[
+            [56.2572, 43.9710],[56.2583, 43.9663],[56.2635,43.9700],[56.2848,43.9697],
+            [56.2843, 43.9805], [56.2735, 43.9759],[56.2572, 43.9710]
+        ],
+        color='green',
+        fill=True,
+        tooltip='Парк Швейцария'
+    ).add_to(parks_and_recreation)
+    sormovski_park = folium.GeoJson(data={ 
+    "type": "Polygon",
+    "coordinates": [
+       [ [43.8665, 56.3380], [43.8653, 56.3334], [43.8617, 56.3320], [43.8458, 56.3387],
+       [43.8522, 56.3449], [43.8655, 56.3419], [43.8665, 56.3380] ]    
+    ]},
+    tooltip = 'Сормовский Парк',
+    style_function=style_geo_json,
     ).add_to(parks_and_recreation)
 
     nn.add_child(homes)  
     nn.add_child(parks_and_recreation)
+
     folium.TileLayer('Stamen Toner').add_to(nn)
     folium.TileLayer('Stamen Terrain').add_to(nn)
 
     folium.LayerControl().add_to(nn)
-    
+
     nn.save('templates/map_nn_auto.html')
     return render(request, 'map_nn_auto.html')
